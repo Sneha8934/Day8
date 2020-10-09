abadc673be92eeba361b29c726f2ff1cbd3dfb2b
Sneha8934 <snehamane004@gmail.com>
2020-10-09 15:13:52 +0530

    ADDED:Day 8 Assignment

---
+++ dictionaryDice.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+ 
+declare -A face_value
+read -p "Enter how many times you want to roll a die : " rollDieTimes
+isRoll=0;
+FACE_ONE=1;
+FACE_TWO=2;
+FACE_THREE=3;
+FACE_FOUR=4;
+FACE_FIVE=5;
+FACE_SIX=6;
+
+while [[ isRoll -lt rollDieTimes ]]
+do
+	roll=$(( RANDOM%6 + 1 ))
+	case $roll in
+		$FACE_ONE)
+			face="One"
+			;;
+		$FACE_TWO)
+			face="Two"
+			;;
+		$FACE_THREE)
+			face="Three"
+			;;
+		$FACE_FOUR)
+			face="Four"
+			;;
+		$FACE_FIVE)
+			face="Five"
+			;;
+		$FACE_SIX)
+			face="Six"
+			;;
+		*)
+			face="Unpredictable situation happen"
+			;;
+	esac
+	face_value[$face]=$roll
+	isRoll=$(( isRoll + 1 ))
+done
+echo "${face_value[@]}"
+
---
+++ prob8.2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+declare -A birthDay
+echo "${birthDay[@]}"
+
+index=1
+while [ $index -le 50 ]
+do
+	random=$((RANDOM%12+1))
+	birthDay[$random]=$((birthDay[$random]+1))
+		((index++))
+done
+
+echo "Individuals Having Birthdays in the Same Month."
+echo "${birthDay[@]}"
